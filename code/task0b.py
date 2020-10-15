4a485e9b75989d92aed7af9ca525e33c6a32a1a5
HarshitaPaila <hpaila@asu.edu>
2020-10-15 15:14:21 -0700

    Code fix

--- code/task0b.py
+++ code/task0b.py
@@ -3,54 +3,58 @@ import os
 import numpy as np
 import pandas as pd
 import csv
-import glob
 
-#This is the total number of unique features in our dataset, so our vector size will be this for every database object or query object
+OUTPUT_FOLDER = "./outputs/vectors/"
+
+# This is the total number of unique features in our dataset, so our vector size will be this for every database
+# object or query object
 feature_dict = set()
 
-#This is a list of dictionaries, each dictionary contains words of one gesture and their respective counts
+# This is a list of dictionaries, each dictionary contains words of one gesture and their respective counts
 words_in_object_map = {}
 
-#This is the total number of words in a given sensor (used in the denominator when calculating TF values)
+# This is the total number of words in a given sensor (used in the denominator when calculating TF values)
 total_count_of_words_in_object = {}
 
-#Getting the number of files in the given directory of gestures (this gives us the number of objects in our database)
+
+# Getting the number of files in the given directory of gestures (this gives us the number of objects in our database)
 def get_number_of_gesture_files_in_dir(directory_path):
-    return len(glob.glob1(directory_path,"*.csv"))
+    return len([x for x in os.listdir(directory_path) if x.endswith(".csv")])
+
 
 def process_word_file(directory_path, file_name):
     file_path = directory_path + file_name
-    words = np.array(pd.read_csv(file_path, header = None))
+    words = np.array(pd.read_csv(file_path, header=None))
     total_count_of_words_in_object[file_name] = len(words)
     words_in_object_map[file_name] = {}
-
     for row in words:
-        sensor_id = row[1]
-        word = [row[0], row[2]]
-        word.extend(row[6:])
+        word = [row[0], row[1]]  # component id, sensor id
+        word.extend(row[5:])  # quantized values
         word = tuple(word)
         feature_dict.add(word)
         if word not in words_in_object_map[file_name]:
             words_in_object_map[file_name][word] = 0
         words_in_object_map[file_name][word] += 1
 
-def create_tf_vectors(directory_path, feature_list):
-    #Creating TF vectors
-    tf_vector_output_file = open(directory_path + "vectors/tf_vectors.csv", "w")
+
+def create_tf_vectors(feature_list):
+    # Creating TF vectors
+    tf_vector_output_file = open(OUTPUT_FOLDER + "tf_vectors.csv", "w")
     csv_write = csv.writer(tf_vector_output_file)
     for file_name, words_map in words_in_object_map.items():
         tf_vector = [file_name]
         for feature in feature_list:
             if feature in words_map:
-                tf_vector.append(words_map[feature]/total_count_of_words_in_object[file_name])
+                tf_vector.append(words_map[feature] / total_count_of_words_in_object[file_name])
             else:
                 tf_vector.append(0)
         csv_write.writerow(tf_vector)
     tf_vector_output_file.close()
 
-def create_tf_idf_vectors(directory_path, gestures_dir, feature_list):
+
+def create_tf_idf_vectors(gestures_dir, feature_list):
     number_of_files = get_number_of_gesture_files_in_dir(gestures_dir + "W/")
-    print(number_of_files)
+    # print("total number of files ", number_of_files)
 
     count_of_a_feature_in_object = []
     for feature in feature_list:
@@ -59,18 +63,17 @@ def create_tf_idf_vectors(directory_path, gestures_dir, feature_list):
             if feature in words_map:
                 count += 1
         count_of_a_feature_in_object.append(count)
-    
-    #Creating TF-IDF vectors
-    tf_vectors = np.array(pd.read_csv(directory_path + "vectors/tf_vectors.csv", header = None))
+
+    # Creating TF-IDF vectors
+    tf_vectors = np.array(pd.read_csv(OUTPUT_FOLDER + "tf_vectors.csv", header=None))
     idf_vector = np.array(count_of_a_feature_in_object)
     idf_vector = np.divide(number_of_files, idf_vector)
     idf_vector = np.log(idf_vector)
-    # print(idf_vector)
-    # max_idf = max(idf_vector)
-    # idf_vector = np.divide(idf_vector, max_idf)
+    # print(np.any((idf_vector == 0))) # VERIFY IF ANY VALUE EQUALS ZERO
     tf_idf_vectors = tf_vectors[0:, 1:] * idf_vector
     tf_idf_vectors = np.hstack((tf_vectors[0:, :1], tf_idf_vectors))
-    pd.DataFrame(tf_idf_vectors).to_csv(directory_path + "vectors/tf_idf_vectors.csv", header = None, index = None)
+    pd.DataFrame(tf_idf_vectors).to_csv(OUTPUT_FOLDER + "tf_idf_vectors.csv", header=None, index=None)
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Create vector models.')
@@ -78,22 +81,22 @@ if __name__ == '__main__':
     parser.add_argument('--output_dir', help='output directory', required=True)
     args = parser.parse_args()
 
-    words_dir_path = args.output_dir + "words/"
+    words_dir_path = args.output_dir
     files = os.listdir(words_dir_path)
     for file_name in files:
         if file_name.endswith(".csv"):
             process_word_file(words_dir_path, file_name)
-    
-    print(len(feature_dict))
+
+    print("feature dict len ", len(feature_dict))
 
     feature_list = list(feature_dict)
     feature_list.sort()
-    
-    #creating vectors directory
+
+    # creating vectors directory
     try:
-        os.mkdir(args.output_dir + "vectors/")
+        os.mkdir(OUTPUT_FOLDER)
     except OSError as error:
         print("vectors directory already exists")
 
-    create_tf_vectors(args.output_dir, feature_list)
+    create_tf_vectors(feature_list)
     create_tf_idf_vectors(args.output_dir, args.gestures_dir, feature_list)
