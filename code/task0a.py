ab1a7ee4554abcccd154f4d207b3a4d04c760526
Rahul Sanjay Avvaru <rahulsanjay@Rahuls-MacBook-Pro.local>
2020-10-14 20:55:39 -0700

    task0a completed, changing the directory of sample data

--- code/task0a.py
+++ code/task0a.py
@@ -10,7 +10,27 @@ MEAN = 0  # from Phase 1
 STANDARD_DEVIATION = 0.25  # from Phase 1
 OUTPUT_FOLDER = "./Outputs"
 OUTPUT_FILE_PATH = "./Outputs/words"
+band_ranges = []
+feature_dict = set()
 
+def getBandNumber(normalized_value):
+    for i in range(len(band_ranges)):
+        if normalized_value >= band_ranges[i][0] and normalized_value <= band_ranges[i][1]:
+            return i+1
+    return -1
+
+def generate_band_ranges(r):
+    start = -1.0
+    total_area = scipy.stats.norm(0, 0.25).cdf(1) - scipy.stats.norm(0, 0.25).cdf(-1)
+    for i in range(1, 2*r+1):
+        lower_bound = (i-r-1)/r
+        upper_bound = (i-r)/r
+        len1 = (scipy.stats.norm(0, 0.25).cdf(upper_bound) - scipy.stats.norm(0, 0.25).cdf(lower_bound))/total_area
+        if i == 2*r:
+            band_ranges.append((start, 1.0))
+        else:
+            band_ranges.append((start, start+2*len1))
+        start = start + 2*len1
 
 def task1():
     parser = argparse.ArgumentParser(description='Create gesture words dictionary.')
@@ -28,28 +48,29 @@ def task1():
             print("Creation of the directory %s failed, because it already exists" % OUTPUT_FOLDER)
         else:
             print("Successfully created the directory %s" % OUTPUT_FOLDER)
+    generate_band_ranges(args.resolution)
 
     dir_list = os.listdir(args.dir)
     for directory in dir_list:
-        if directory in ['X']:
-            component_id = directory
-            files = os.listdir(args.dir+directory)
-            print(files)
-            for file in files:
-                if ".csv" in file:
-                    sensor_data = pd.DataFrame(pd.read_csv(args.dir + directory + '/'+ file, header=None)).to_numpy()
-                    data_avg = np.mean(sensor_data, axis=1)
-                    # print(data_avg)
-                    data_std = np.std(sensor_data, axis=1)
-                    # print(data_std)
-                    normalized_data = normalize(sensor_data)
-                    # print(normalized_data.shape)
-                    quantized_data = gaussian_transform(pd.DataFrame(normalized_data), get_gaussian_bands(args.resolution), args.resolution)
-                    # print(sensor_quantized_data)
-                    get_window_avg(normalized_data, args.window, args.shift)
-                    get_quantized_window(quantized_data, args.window, args.shift)
-                    break
-                    #         write_words(quantized_data, args.shift, args.window, f)
+        component_id = directory
+        files = os.listdir(args.dir+directory)
+        for file in files:
+            if ".csv" in file:
+                sensor_data = pd.DataFrame(pd.read_csv(args.dir + directory + '/'+ file, header=None)).to_numpy()
+                data_avg = np.mean(sensor_data, axis=1)
+                # print(data_avg)
+                data_std = np.std(sensor_data, axis=1)
+                # print(data_std)
+                normalized_data = normalize(sensor_data)
+                # print(normalized_data.shape)
+                quantized_data = np.vectorize(getBandNumber)(normalized_data)
+                # print(sensor_quantized_data)
+                # get_window_avg(normalized_data, args.window, args.shift)
+                get_quantized_window(quantized_data, args.window, args.shift)
+                write_words(normalized_data, quantized_data, args.shift, args.window, file, component_id, data_avg, data_std)
+    
+    print(len(feature_dict))
+
 
 def get_quantized_window(data, window, shift):
     for i, row in enumerate(data):
@@ -58,7 +79,7 @@ def get_quantized_window(data, window, shift):
             # print("h = ", h, " window values ", row[h:h+window])
             temp = [i, h, row[h:h+window].tolist()]
             h += shift
-            print(temp)
+            # print(temp)
         break
 
 def get_window_avg(data, window, shift):
@@ -69,13 +90,12 @@ def get_window_avg(data, window, shift):
             val = row[h:h+window].tolist()
             temp = [i, h, mean(val)]
             h += shift
-            print(temp)
+            # print(temp)
         break
 
-
-def write_words(d, shift, window, file):
-    file_no = file.replace(".csv", "")
-    data = d.to_numpy()
+def write_words(normalized_data, quantized_data, shift, window, file, component_id, data_avg, data_std):
+    file_name = file.replace(".csv", "")
+    data = quantized_data
     max_time = len(data[0])
     max_sensors = len(data)
     sensor = 0
@@ -86,15 +106,18 @@ def write_words(d, shift, window, file):
             print("Creation of the directory %s failed, because it already exists" % OUTPUT_FILE_PATH)
         else:
             print("Successfully created the directory %s" % OUTPUT_FILE_PATH)
-    file_name = OUTPUT_FILE_PATH + "/" + file_no + "words.csv"
-    with open(file_name, mode='w') as output:
+    output_file_name = OUTPUT_FILE_PATH + "/" + file_name + "_words.csv"
+    with open(output_file_name, mode='a') as output:
         fp = csv.writer(output, delimiter=',')
         while sensor < max_sensors:
             for t in range(0, max_time, shift):
-                row = [file_no, sensor, t]
+                row = [component_id, file, sensor, data_avg[sensor], data_std[sensor], mean(normalized_data[sensor][t:t+window].tolist())]
                 row += data[sensor][t:t + window].tolist()
-                if len(row) == 3 + window:
+                if len(row) == 6 + window:
                     # prune words which do not have enough data
+                    word = [component_id, sensor]
+                    word.extend(row[6:])
+                    feature_dict.add(tuple(word))
                     fp.writerow(row)
             sensor += 1
 
@@ -111,11 +134,12 @@ def normalize(data):
 def get_gaussian_bands(resolution):
     r = resolution
     gaussian_bands = [-1]
+    total_area = scipy.stats.norm(0, 0.25).cdf(1) - scipy.stats.norm(0, 0.25).cdf(-1)
     for i in range(1, 2 * r + 1):
         lower_bound = (i - r - 1) / r
         upper_bound = (i - r) / r
         gaussian_bands.append(gaussian_bands[-1] + 2 * (scipy.stats.norm(MEAN, STANDARD_DEVIATION).cdf(upper_bound) -
-                                                        scipy.stats.norm(MEAN, STANDARD_DEVIATION).cdf(lower_bound)))
+                                                        scipy.stats.norm(MEAN, STANDARD_DEVIATION).cdf(lower_bound))/total_area)
     gaussian_bands[-1] = 1
     return gaussian_bands
 
