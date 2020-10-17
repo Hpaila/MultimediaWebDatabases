4175a18da22ef169a7a3ffc6bf72a0e73c75f0eb
Rahul Sanjay Avvaru <rahulsanjay@Rahuls-MacBook-Pro.local>
2020-10-17 02:20:54 -0700

    Revert "Code fix"
    This reverts commit 4a485e9b75989d92aed7af9ca525e33c6a32a1a5.

--- code/task0a.py
+++ code/task0a.py
@@ -13,13 +13,11 @@ OUTPUT_FILE_PATH = "./outputs/words"
 band_ranges = []
 feature_dict = set()
 
-
-def get_band_number(normalized_value):
+def getBandNumber(normalized_value):
     for i in range(len(band_ranges)):
-        if band_ranges[i][0] <= normalized_value <= band_ranges[i][1]:
+        if normalized_value >= band_ranges[i][0] and normalized_value <= band_ranges[i][1]:
             return i+1
     return -1
-
 
 def generate_band_ranges(r):
     start = -1.0
@@ -34,48 +32,7 @@ def generate_band_ranges(r):
             band_ranges.append((start, start+2*len1))
         start = start + 2*len1
 
-
-def write_words(normalized_data, quantized_data, shift, window, file, component_id, data_avg, data_std):
-    file_name = file.replace(".csv", "")
-    data = quantized_data
-    max_time = len(data[0])
-    max_sensors = len(data)
-    sensor = 0
-    if not os.path.isdir(OUTPUT_FILE_PATH):
-        try:
-            os.mkdir(OUTPUT_FILE_PATH)
-        except OSError:
-            print("Creation of the directory %s failed, because it already exists" % OUTPUT_FILE_PATH)
-        else:
-            print("Successfully created the directory %s" % OUTPUT_FILE_PATH)
-    output_file_name = OUTPUT_FILE_PATH + "/" + file_name + "_words.csv"
-    with open(output_file_name, mode='a') as output:
-        fp = csv.writer(output, delimiter=',')
-        while sensor < max_sensors:
-            for t in range(0, max_time, shift):
-                row = [component_id, sensor, data_avg[sensor], data_std[sensor], mean(normalized_data[sensor][t:t+window].tolist())]
-                row += data[sensor][t:t + window].tolist()
-                if len(row) == 5 + window:
-                    # prune words which do not have enough data
-                    word = [component_id, sensor]
-                    word.extend(row[5:])
-                    feature_dict.add(tuple(word))
-                    fp.writerow(row)
-            sensor += 1
-
-
-def normalize(data):
-    data = np.transpose(data)
-    if np.all(data == data[0]):  # If all values are same, normalize to zero
-        new_data = np.zeros(data.shape)
-    else:
-        new_data = np.divide(data - data.min(axis=0), data.max(axis=0) - data.min(axis=0), out=data, where=(data.max(axis=0) - data.min(axis=0)) != 0)
-        new_data = (new_data * 2) - 1
-    new_data = np.transpose(new_data)
-    return new_data
-
-
-if __name__ == '__main__':
+def task1():
     parser = argparse.ArgumentParser(description='Create gesture words dictionary.')
     parser.add_argument('--dir', help='directory of input data', required=True)
     parser.add_argument('--window', type=int, help='window length', required=True)
@@ -96,17 +53,107 @@ if __name__ == '__main__':
     dir_list = os.listdir(args.dir)
     for directory in dir_list:
         component_id = directory
-        files = []
-        if component_id in ['X', 'Y', 'Z', 'W']:  # To Exclude .DS_store files on Mac
-            files = os.listdir(args.dir + directory)
+        files = os.listdir(args.dir+directory)
         for file in files:
             if ".csv" in file:
-                sensor_data = pd.DataFrame(pd.read_csv(args.dir + directory + '/' + file, header=None)).to_numpy()
+                sensor_data = pd.DataFrame(pd.read_csv(args.dir + directory + '/'+ file, header=None)).to_numpy()
                 data_avg = np.mean(sensor_data, axis=1)
+                # print(data_avg)
                 data_std = np.std(sensor_data, axis=1)
+                # print(data_std)
                 normalized_data = normalize(sensor_data)
-                quantized_data = np.vectorize(get_band_number)(normalized_data)
-                write_words(normalized_data, quantized_data, args.shift, args.window, file, component_id, data_avg,
-                            data_std)
-
+                # print(normalized_data.shape)
+                quantized_data = np.vectorize(getBandNumber)(normalized_data)
+                # print(sensor_quantized_data)
+                # get_window_avg(normalized_data, args.window, args.shift)
+                get_quantized_window(quantized_data, args.window, args.shift)
+                write_words(normalized_data, quantized_data, args.shift, args.window, file, component_id, data_avg, data_std)
+    
     print(len(feature_dict))
+
+
+def get_quantized_window(data, window, shift):
+    for i, row in enumerate(data):
+        h = 0
+        while h + window < len(row):
+            # print("h = ", h, " window values ", row[h:h+window])
+            temp = [i, h, row[h:h+window].tolist()]
+            h += shift
+            # print(temp)
+        break
+
+def get_window_avg(data, window, shift):
+    for i, row in enumerate(data):
+        h = 0
+        while h + window < len(row):
+            # print("h = ", h, " window values ", row[h:h+window])
+            val = row[h:h+window].tolist()
+            temp = [i, h, mean(val)]
+            h += shift
+            # print(temp)
+        break
+
+def write_words(normalized_data, quantized_data, shift, window, file, component_id, data_avg, data_std):
+    file_name = file.replace(".csv", "")
+    data = quantized_data
+    max_time = len(data[0])
+    max_sensors = len(data)
+    sensor = 0
+    if not os.path.isdir(OUTPUT_FILE_PATH):
+        try:
+            os.mkdir(OUTPUT_FILE_PATH)
+        except OSError:
+            print("Creation of the directory %s failed, because it already exists" % OUTPUT_FILE_PATH)
+        else:
+            print("Successfully created the directory %s" % OUTPUT_FILE_PATH)
+    output_file_name = OUTPUT_FILE_PATH + "/" + file_name + "_words.csv"
+    with open(output_file_name, mode='a') as output:
+        fp = csv.writer(output, delimiter=',')
+        while sensor < max_sensors:
+            for t in range(0, max_time, shift):
+                row = [component_id, file, sensor, data_avg[sensor], data_std[sensor], mean(normalized_data[sensor][t:t+window].tolist())]
+                row += data[sensor][t:t + window].tolist()
+                if len(row) == 6 + window:
+                    # prune words which do not have enough data
+                    word = [component_id, sensor]
+                    word.extend(row[6:])
+                    feature_dict.add(tuple(word))
+                    fp.writerow(row)
+            sensor += 1
+
+
+def normalize(data):
+    data = np.transpose(data)
+    new_data = np.divide(data - data.min(axis=0), data.max(axis=0) - data.min(axis=0), out=data,
+                         where=(data.max(axis=0) - data.min(axis=0)) != 0)
+    new_data = (new_data * 2) - 1
+    new_data = np.transpose(new_data)
+    return new_data
+
+
+def get_gaussian_bands(resolution):
+    r = resolution
+    gaussian_bands = [-1]
+    total_area = scipy.stats.norm(0, 0.25).cdf(1) - scipy.stats.norm(0, 0.25).cdf(-1)
+    for i in range(1, 2 * r + 1):
+        lower_bound = (i - r - 1) / r
+        upper_bound = (i - r) / r
+        gaussian_bands.append(gaussian_bands[-1] + 2 * (scipy.stats.norm(MEAN, STANDARD_DEVIATION).cdf(upper_bound) -
+                                                        scipy.stats.norm(MEAN, STANDARD_DEVIATION).cdf(lower_bound))/total_area)
+    gaussian_bands[-1] = 1
+    return gaussian_bands
+
+
+def gaussian_transform(data, bands, resolution):
+    def get_band_number(x):
+        for i in range(len(bands) - 1):
+            if bands[i] <= x <= bands[i + 1]:
+                return i + 1
+        if x > bands[-1]:
+            return 2 * resolution
+
+    return data.transform([get_band_number])
+
+
+if __name__ == '__main__':
+    task1()
--- code/task0b.py
+++ code/task0b.py
@@ -3,58 +3,54 @@ import os
 import numpy as np
 import pandas as pd
 import csv
+import glob
 
-OUTPUT_FOLDER = "./outputs/vectors/"
-
-# This is the total number of unique features in our dataset, so our vector size will be this for every database
-# object or query object
+#This is the total number of unique features in our dataset, so our vector size will be this for every database object or query object
 feature_dict = set()
 
-# This is a list of dictionaries, each dictionary contains words of one gesture and their respective counts
+#This is a list of dictionaries, each dictionary contains words of one gesture and their respective counts
 words_in_object_map = {}
 
-# This is the total number of words in a given sensor (used in the denominator when calculating TF values)
+#This is the total number of words in a given sensor (used in the denominator when calculating TF values)
 total_count_of_words_in_object = {}
 
-
-# Getting the number of files in the given directory of gestures (this gives us the number of objects in our database)
+#Getting the number of files in the given directory of gestures (this gives us the number of objects in our database)
 def get_number_of_gesture_files_in_dir(directory_path):
-    return len([x for x in os.listdir(directory_path) if x.endswith(".csv")])
-
+    return len(glob.glob1(directory_path,"*.csv"))
 
 def process_word_file(directory_path, file_name):
     file_path = directory_path + file_name
-    words = np.array(pd.read_csv(file_path, header=None))
+    words = np.array(pd.read_csv(file_path, header = None))
     total_count_of_words_in_object[file_name] = len(words)
     words_in_object_map[file_name] = {}
+
     for row in words:
-        word = [row[0], row[1]]  # component id, sensor id
-        word.extend(row[5:])  # quantized values
+        sensor_id = row[1]
+        word = [row[0], row[2]]
+        word.extend(row[6:])
         word = tuple(word)
         feature_dict.add(word)
         if word not in words_in_object_map[file_name]:
             words_in_object_map[file_name][word] = 0
         words_in_object_map[file_name][word] += 1
 
-
-def create_tf_vectors(feature_list):
-    # Creating TF vectors
-    tf_vector_output_file = open(OUTPUT_FOLDER + "tf_vectors.csv", "w")
+def create_tf_vectors(directory_path, feature_list):
+    #Creating TF vectors
+    tf_vector_output_file = open(directory_path + "vectors/tf_vectors.csv", "w")
     csv_write = csv.writer(tf_vector_output_file)
     for file_name, words_map in words_in_object_map.items():
         tf_vector = [file_name]
         for feature in feature_list:
             if feature in words_map:
-                tf_vector.append(words_map[feature] / total_count_of_words_in_object[file_name])
+                tf_vector.append(words_map[feature]/total_count_of_words_in_object[file_name])
             else:
                 tf_vector.append(0)
         csv_write.writerow(tf_vector)
     tf_vector_output_file.close()
 
-
-def create_tf_idf_vectors(gestures_dir, feature_list):
+def create_tf_idf_vectors(directory_path, gestures_dir, feature_list):
     number_of_files = get_number_of_gesture_files_in_dir(gestures_dir + "W/")
-    # print("total number of files ", number_of_files)
+    print(number_of_files)
 
     count_of_a_feature_in_object = []
     for feature in feature_list:
@@ -63,17 +59,18 @@ def create_tf_idf_vectors(gestures_dir, feature_list):
             if feature in words_map:
                 count += 1
         count_of_a_feature_in_object.append(count)
-
-    # Creating TF-IDF vectors
-    tf_vectors = np.array(pd.read_csv(OUTPUT_FOLDER + "tf_vectors.csv", header=None))
+    
+    #Creating TF-IDF vectors
+    tf_vectors = np.array(pd.read_csv(directory_path + "vectors/tf_vectors.csv", header = None))
     idf_vector = np.array(count_of_a_feature_in_object)
     idf_vector = np.divide(number_of_files, idf_vector)
     idf_vector = np.log(idf_vector)
-    # print(np.any((idf_vector == 0))) # VERIFY IF ANY VALUE EQUALS ZERO
+    # print(idf_vector)
+    # max_idf = max(idf_vector)
+    # idf_vector = np.divide(idf_vector, max_idf)
     tf_idf_vectors = tf_vectors[0:, 1:] * idf_vector
     tf_idf_vectors = np.hstack((tf_vectors[0:, :1], tf_idf_vectors))
-    pd.DataFrame(tf_idf_vectors).to_csv(OUTPUT_FOLDER + "tf_idf_vectors.csv", header=None, index=None)
-
+    pd.DataFrame(tf_idf_vectors).to_csv(directory_path + "vectors/tf_idf_vectors.csv", header = None, index = None)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Create vector models.')
@@ -81,22 +78,22 @@ if __name__ == '__main__':
     parser.add_argument('--output_dir', help='output directory', required=True)
     args = parser.parse_args()
 
-    words_dir_path = args.output_dir
+    words_dir_path = args.output_dir + "words/"
     files = os.listdir(words_dir_path)
     for file_name in files:
         if file_name.endswith(".csv"):
             process_word_file(words_dir_path, file_name)
-
-    print("feature dict len ", len(feature_dict))
+    
+    print(len(feature_dict))
 
     feature_list = list(feature_dict)
     feature_list.sort()
-
-    # creating vectors directory
+    
+    #creating vectors directory
     try:
-        os.mkdir(OUTPUT_FOLDER)
+        os.mkdir(args.output_dir + "vectors/")
     except OSError as error:
         print("vectors directory already exists")
 
-    create_tf_vectors(feature_list)
+    create_tf_vectors(args.output_dir, feature_list)
     create_tf_idf_vectors(args.output_dir, args.gestures_dir, feature_list)
