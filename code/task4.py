35342deee22059f289a59ca6cb1a0e253ea1e46f
Rahul Sanjay Avvaru <rahulsanjay@Rahuls-MacBook-Pro.local>
2020-10-24 18:49:32 -0700

    adding number of clusters parameter in task 4 and reading the similarity matrix

--- code/task4.py
+++ code/task4.py
@@ -1,5 +1,6 @@
 import argparse
 import numpy as np
+import pandas as pd
 
 def partition_gestures_into_p_groups(svd_or_nmf_type):
     with open(args.output_dir + "top_p_latent_gestures_scores_" + svd_or_nmf_type + "_" + str(args.user_option)) as f:
@@ -22,14 +23,23 @@ def partition_gestures_into_p_groups(svd_or_nmf_type):
     
     return partition_map
 
+def perform_laplacian_spectral_clustering(similarity_matrix, num_of_clusters):
+    pass
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Create vector models.')
     parser.add_argument('--output_dir', help='output directory', required=True)
     parser.add_argument('--user_option', type=int, help='user option', required=True)
+    parser.add_argument('--p', type=int, help='Number of clusters', required=True)
     args = parser.parse_args()
-
+    
     # Task 4a - Using SVD from 3a
     print(partition_gestures_into_p_groups("svd"))
     
     # Task 4b - Using NMF from 3b
-    print(partition_gestures_into_p_groups("nmf"))
+    # print(partition_gestures_into_p_groups("nmf"))
+
+    # Laplacian spectral clustering 
+    similarity_matrix_with_headers = np.array(pd.read_csv(args.output_dir + "similarity_matrix_" + str(args.user_option) + ".csv", header=None))
+    similarity_matrix = similarity_matrix_with_headers[1:, 1:]
+    perform_laplacian_spectral_clustering(similarity_matrix, args.p)
