c25c9bd46b7a5ed5c41541f76f084480b12f3ad3
Rahul Sanjay Avvaru <rahulsanjay@Rahuls-MacBook-Pro.local>
2020-11-23 02:20:12 -0700

    Task6 with GUI

--- phase3/task4.py
+++ phase3/task4.py
@@ -9,6 +9,53 @@ def convert(val):
         return 1
     return 0
 
+def get_updated_gestures(relevant_gestures, t, initial_search_results):
+    #By default, using tf_idf vectors
+    input_vectors_df = pd.read_csv("outputs/vectors/tf_idf_vectors.csv")
+    columns = input_vectors_df.columns[1:]
+    columns = [eval(column) for column in columns]
+
+    input_vectors_with_labels = np.array(input_vectors_df)
+    input_vectors = input_vectors_with_labels[0:, 1:]
+    N = len(input_vectors)
+    d = input_vectors.shape[1]
+    
+    binary_weights_matrix = np.vectorize(convert)(input_vectors)
+    
+    input_vectors_map = {}
+    for i in range(len(input_vectors_with_labels)):
+        input_vectors_map[input_vectors_with_labels[i][0]] = input_vectors[i]
+
+    binary_vectors_map = {}
+    for i in range(len(input_vectors_with_labels)):
+        binary_vectors_map[input_vectors_with_labels[i][0]] = binary_weights_matrix[i]
+
+    similarity_map = {}
+    N = t
+    R = len(relevant_gestures)
+    
+    r = np.array([0 for i in range(d)])
+    for relevant_gesture in relevant_gestures:
+        r += binary_vectors_map[relevant_gesture]
+    
+    n = np.array([0 for i in range(d)])
+    for i in range(t):
+        n += binary_vectors_map[initial_search_results[i]]
+    
+    factor = n/N
+    p = np.divide(r + 0.5, R+1)
+    u = np.divide(n - r + 0.5, N-R+1)
+    log_values = np.log(np.divide(p*(1-u), u*(1-p)))
+
+    for i in range(len(input_vectors)):
+        similarity_map[input_vectors_with_labels[i][0]] = np.sum(binary_weights_matrix[i] * log_values)
+
+    sort_orders = sorted(similarity_map.items(), key=lambda x: x[1], reverse=True)
+    search_results = []
+    for i in range(0,t):
+        search_results.append(sort_orders[i][0])
+    return search_results
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Probabilistic relevance feedback')
     parser.add_argument('--query_gesture', help='query gesture', required=True)
