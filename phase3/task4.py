5f5d3cb49ba01cd623ff74e7ed3fa4333031516a
Rahul Sanjay Avvaru <rahulsanjay@Rahuls-MacBook-Pro.local>
2020-11-22 23:52:37 -0700

    calling task3 search in task4

--- phase3/task4.py
+++ phase3/task4.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 import argparse
 from scipy.spatial import distance
+from task3 import get_t_closest_gestures
 
 def convert(val):
     if val > 0:
@@ -15,6 +16,13 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     query_gesture = args.query_gesture + "_words.csv"
+    
+    #initial run
+    initial_search_results = get_t_closest_gestures(6, 3, "outputs/vectors/tf_idf_vectors.csv", args.t, query_gesture)
+
+    print("Initial search results are")
+    for result in initial_search_results:
+        print(result)
 
     #By default, using tf_idf vectors
     input_vectors_df = pd.read_csv("outputs/vectors/tf_idf_vectors.csv")
@@ -36,16 +44,7 @@ if __name__ == '__main__':
     for i in range(len(input_vectors_with_labels)):
         binary_vectors_map[input_vectors_with_labels[i][0]] = binary_weights_matrix[i]
 
-    #initial_run
     similarity_map = {}
-    for gesture in input_vectors_map:
-        similarity_map[gesture] = np.dot(input_vectors_map[query_gesture], input_vectors_map[gesture])
-
-    sort_orders = sorted(similarity_map.items(), key=lambda x: x[1], reverse=True)
-
-    print("Initial results are")
-    for i in range(0,args.t):
-        print(sort_orders[i][0])
 
     while True:
         relevant_gestures = input("Enter the relevant gestures if any\n")
@@ -54,6 +53,7 @@ if __name__ == '__main__':
         else:
             relevant_gestures = relevant_gestures.split(" ")
             relevant_gestures = [gesture + "_words.csv" for gesture in relevant_gestures]
+
         R = len(relevant_gestures)
     
         r = np.array([0 for i in range(d)])
@@ -62,12 +62,11 @@ if __name__ == '__main__':
         
         n = np.array([0 for i in range(d)])
         for i in range(args.t):
-            n += binary_vectors_map[sort_orders[i][0]]
+            n += binary_vectors_map[initial_search_results[i]]
         
         factor = n/N
         p = np.divide(r + 0.5, R+1)
         u = np.divide(n - r + 0.5, N-R+1)
-
         log_values = np.log(np.divide(p*(1-u), u*(1-p)))
 
         for i in range(len(input_vectors)):
@@ -75,5 +74,7 @@ if __name__ == '__main__':
 
         sort_orders = sorted(similarity_map.items(), key=lambda x: x[1], reverse=True)
         print("Modified results are")
+        initial_search_results = []
         for i in range(0,args.t):
+            initial_search_results.append(sort_orders[i][0])
             print(sort_orders[i][0])
