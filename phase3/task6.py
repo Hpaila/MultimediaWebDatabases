1c1f954f9c2c2524a8daab391b43f5accff55825
SudiniAkshitha <asudini@asu.edu>
2020-11-23 19:13:20 -0700

    Added ppr feedback for task6

--- phase3/task6.py
+++ phase3/task6.py
@@ -2,14 +2,16 @@
 from appJar import gui
 from task3 import get_t_closest_gestures
 from task4 import get_updated_gestures
-from task5 import get_updated_gestures_task5
+from task5 import get_updated_gestures_task5, initial_result_task5
 
 app = None
 t = None
 feedback_type = None
+query_gesture = None
 def submit_feedback():
     global app
     global t
+    global query_gesture
     relevant_gestures = []
     irrelevant_gestures = []
     all_results = []
@@ -25,7 +27,7 @@ def submit_feedback():
         updated_results = get_updated_gestures(relevant_gestures, int(t), all_results)
     else:
         print("Calling PPR Feedback")
-        # updated_results = get_updated_gestures_task5(relevant_gestures, irrelevant_gestures, int(t))
+        updated_results = get_updated_gestures_task5(relevant_gestures, irrelevant_gestures, int(t), query_gesture)
     app.stop()
     app = gui("Query interface")
     app.addLabel("l1", "Updated Query results")
@@ -45,12 +47,14 @@ def search():
     global app
     global t
     global feedback_type
+    global query_gesture
     query_gesture = app.getEntry("Enter the gesture")
     query_gesture = query_gesture + "_words.csv"
 
     t = app.getEntry("Enter the number of results to be returned")
 
-    initial_search_results = get_t_closest_gestures(6, 3, "outputs/vectors/tf_idf_vectors.csv", int(t), query_gesture)
+
+    initial_search_results = initial_result_task5("../outputs/tf_idf_pca_vectors.csv", int(t), query_gesture)
     feedback_type = app.getRadioButton("relevance_feedback_type")
     app.stop()
     app = gui("Query interface")
